ec5ca3dda6491473e9a58b9211ab6b4eda0c56a4
Robert Schieblich <rob128@hotmail.de>
2018-05-12 15:33:31 +0200

    - Added SignInActivity to the application

--- app/src/main/java/com/rob128/fishtracker/signin/SignInActivity.java
+++ app/src/main/java/com/rob128/fishtracker/signin/SignInActivity.java
@@ -1,7 +1,9 @@
 package com.rob128.fishtracker.signin;
 
+import android.support.v7.app.AppCompatActivity;
+
 /**
  * Created by Robert on 12.05.2018
  */
-public class SignInActivity {
+public class SignInActivity extends AppCompatActivity {
 }
